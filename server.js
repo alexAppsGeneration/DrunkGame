159a7a06dcfd347a507a312b5bea281a7fe3bc63
oleksandrViniar <oleksandr.viniar@taxsystems.com>
2021-05-07 15:56:51 +0300

    update for ionic project

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+//Install express server
 const express = require('express');
+// Requirement of path Module in order to manage URLS
+const path = require('path');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/www'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/www/index.html'));
+});
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
